3781b97d2e8407becefa97e09e8271c2cfe46b49
Oleg Desyatnikov <evilgazz@yandex.ru>
2020-04-03 15:57:59 +0300

    Исправил ошибку конвертации. Сделал проверку на запятую. Округление до 2-х знаков после запятой

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 "use strict"
 
 
-let pharyngateInput = document.getElementById("celsiusInput")
+let celsiusInput = document.getElementById("celsiusInput")
 let buttonPharyngate = document.getElementById("buttonPharyngate")
 let outCelsius = document.querySelector(".main__homework__out")
 
 
 buttonPharyngate.onclick = () => {
 
-		if (pharyngateInput.value !== "") {
+		if (celsiusInput.value !== "") {
 
 			outCelsius.textContent = ""
 			
-			let celsiusTemperature = (9 / 5) * +pharyngateInput.value + 32
+			let pharyngateTemperature = (9 / 5) * +celsiusInput.value.replace(/,/, '.') + 32
 			
 			//через prepend
-			outCelsius.prepend("цельсия: " + celsiusTemperature)
+			outCelsius.prepend("фаренгейт: " + pharyngateTemperature.toFixed(2))
 			
 			//через innerHTML
 			//outCelsius.innerHTML = "цельсия: " + celsiusTemperature
@@ -36,5 +36,3 @@ let name = "Василий"
 admin = name
 
 console.log(admin)
-
-
